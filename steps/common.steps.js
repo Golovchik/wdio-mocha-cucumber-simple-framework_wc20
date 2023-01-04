06894724d99479f955a3836a3fb97023cdf64276
Vlad Golovchik <golo.vv@yandex.ru>
2023-01-04 17:21:15 +0300

    feat: created feature 1

---
+++ steps/common.steps.js
@@ -0,0 +1,27 @@
+const {Given, When, Then} = require('@cucumber/cucumber');
+const {page} = require('../po/pages/index');
+
+// Given/When I open the browser is at the "Dashboard" page
+Given(/^I open the browser is at the '([^"]*)' page$/, async (pageName) => {
+  await page(pageName).open();
+});
+
+// When I click on the "menuItem" item in the menu
+When(/^I click on the '([^"]*)' item in the menu$/, async (menuItem) => {
+  await page('Base').sideMenu.item(menuItem).click();
+});
+
+// Then I should have page url "pageUrl"
+Then(/^I should have page url '([^"]*)'$/, async (url) => {
+  const currentUrl = await page('Base').getPageUrl();
+  await expect(currentUrl).toEqual(url);
+});
+
+// Then I should see modal dialog with title 'New Doctor' on page 'Doctors'
+Then(
+    /^Then I should see modal dialog with title '([^"]*)' on page '([^"]*)'$/,
+    async (title, pageName) => {
+      const currenTitle = await page(pageName).changeDoctorModal.title;
+      await expect(currenTitle).toEqual(title);
+    },
+);
