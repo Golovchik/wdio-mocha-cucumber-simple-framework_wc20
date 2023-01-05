2ecc51af72c8cd1b7f53e1f297cd923eda2226d9
Anna Morozova <tsiganenko.a@yandex.ru>
2023-01-05 17:50:30 +0400

    'add scenarios'

--- steps/common.steps.js
+++ steps/common.steps.js
@@ -1,5 +1,5 @@
-const {Given, When, Then} = require('@cucumber/cucumber');
-const {page} = require('../po/pages/index');
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { page } = require("../po/pages/index");
 
 // Given/When I open the browser is at the "Dashboard" page
 Given(/^I open the browser is at the '([^"]*)' page$/, async (pageName) => {
@@ -8,28 +8,34 @@ Given(/^I open the browser is at the '([^"]*)' page$/, async (pageName) => {
 
 // When I click on the "menuItem" item in the menu
 When(/^I click on the '([^"]*)' item in the menu$/, async (menuItem) => {
-  await page('Base').sideMenu.item(menuItem).click();
+  await page("Base").sideMenu.item(menuItem).click();
 });
 
 // When I click on the element 'el' on page 'pa' on component 'co'
 When(
-    /^I click on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-    async (el, pg, comp) => {
-      await page(pg)[comp].item(el).click();
-    },
+  /^I click on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+  async (el, pg, comp) => {
+    await page(pg)[comp].item(el).click();
+  }
 );
 
 // Then I should have page url "pageUrl"
 Then(/^I should have page url '([^"]*)'$/, async (url) => {
-  const currentUrl = await page('Base').getPageUrl();
+  const currentUrl = await page("Base").getPageUrl();
   await expect(currentUrl).toEqual(url);
 });
 
 // Then I should see modal dialog with title 'New Doctor' on page 'Doctors'
 Then(
-    /^I should see modal dialog with title '([^"]*)' on page '([^"]*)'$/,
-    async (title, pageName) => {
-      const currenTitle = await page(pageName).newDoctorModal.title.getText();
-      await expect(currenTitle).toEqual(title);
-    },
+  /^I should see modal dialog with title '([^"]*)' on page '([^"]*)'$/,
+  async (title, pageName) => {
+    const currenTitle = await page(pageName).newDoctorModal.title.getText();
+    await expect(currenTitle).toEqual(title);
+  }
 );
+
+// Then I write text on element
+Then(/^I write text '([^"]*)' on element'([^"]*)'$/, async (text, element) => {
+  await element.waitForExist(5000);
+  await element.addValue(text);
+});
