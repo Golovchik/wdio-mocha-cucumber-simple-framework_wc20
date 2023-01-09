3c84c2e4d314fd87cca75b3d48db1842805e5b65
Vlad Golovchik <golo.vv@yandex.ru>
2023-01-09 11:35:35 +0300

    fix: deleted comments

--- po/components/patients/clientCard.component.js
+++ po/components/patients/clientCard.component.js
@@ -1,22 +1,20 @@
-const BaseComponent = require('../common/base.component');
+const BaseComponent = require("../common/base.component");
 
 class ClientCardComponent extends BaseComponent {
   constructor(number) {
-    // super(`//tbody/tr/td[text()='${id}']`);
-    // super(`//tbody/tr/td[2]/span[text()='${name}']`);
     super(`//tbody/tr/td[6][text()='${number}']/ancestor::tr`);
   }
 
   item(name) {
     const selectors = {
-      id: 'td[aria-label$=\'ID\']',
-      name: 'td[aria-label$=\'Name\']',
-      nameLink: 'td[aria-label$=\'Name\'] span',
-      gender: 'td[aria-label$=\'Gender\']',
-      bloodGroup: 'td[aria-label$=\'Blood Group\']',
-      symptoms: 'td[aria-label$=\'Symptoms\']',
-      mobileNumber: 'td[aria-label$=\'Mobile Number\']',
-      email: 'td[aria-label$=\'Email\']',
+      id: "td[aria-label$='ID']",
+      name: "td[aria-label$='Name']",
+      nameLink: "td[aria-label$='Name'] span",
+      gender: "td[aria-label$='Gender']",
+      bloodGroup: "td[aria-label$='Blood Group']",
+      symptoms: "td[aria-label$='Symptoms']",
+      mobileNumber: "td[aria-label$='Mobile Number']",
+      email: "td[aria-label$='Email']",
     };
     return this.rootEl.$(selectors[name]);
   }
