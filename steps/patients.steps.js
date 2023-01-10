664c44f055cd701f71ba4e549e51ca820719177d
Anna Morozova <tsiganenko.a@yandex.ru>
2023-01-10 21:55:57 +0400

    check search string

--- steps/patients.steps.js
+++ steps/patients.steps.js
@@ -1,73 +1,73 @@
 /* eslint-disable max-len */
-const { When, Then } = require("@cucumber/cucumber");
-const { getConvertTextToVariable } = require("../support/helpers");
-const { pageFactory } = require("../po/pages/index");
-const { clickElementWithWait } = require("../support/actions/cooperations");
-const { checkElementIsDisplayed } = require("../support/assertions/expects");
+const {When, Then} = require('@cucumber/cucumber');
+const {getConvertTextToVariable} = require('../support/helpers');
+const {pageFactory} = require('../po/pages/index');
+const {clickElementWithWait} = require('../support/actions/cooperations');
+const {checkElementIsDisplayed} = require('../support/assertions/expects');
 
 When(
-  /^I click on the patient with mobile number '([^"]*)' on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-  async (text, element, page, component) => {
-    this.patientMobile = text;
-    element = await getConvertTextToVariable(element);
-    const currentElement = await pageFactory(page)
-      .clientCard(text)
-      .item(element);
-    await clickElementWithWait(currentElement);
-  }
+    /^I click on the patient with mobile number '([^"]*)' on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+    async (text, element, page, component) => {
+      this.patientMobile = text;
+      element = await getConvertTextToVariable(element);
+      const currentElement = await pageFactory(page)
+          .clientCard(text)
+          .item(element);
+      await clickElementWithWait(currentElement);
+    },
 );
 
 // Then I should see for patients 'Ul' into elements 'Name' on page 'Patients' on component 'Client Card'
 Then(
-  /^I should see for patients '([^"]*)' into elements '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-  async (text, element, page, component) => {
-    element = await getConvertTextToVariable(element);
-    const currentElement = await pageFactory(page)
-      .clientCard(this.patientMobile)
-      .item(element);
-    await checkElementIsDisplayed(currentElement);
-    const currentText = await currentElement.getText();
-    await expect(currentText).toEqual(text);
-  }
+    /^I should see for patients '([^"]*)' into elements '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+    async (text, element, page, component) => {
+      element = await getConvertTextToVariable(element);
+      const currentElement = await pageFactory(page)
+          .clientCard(this.patientMobile)
+          .item(element);
+      await checkElementIsDisplayed(currentElement);
+      const currentText = await currentElement.getText();
+      await expect(currentText).toEqual(text);
+    },
 );
 
 // Then I should see text 'Anna' into element 'Name' and text '1111111111' into element 'Mobile' on page 'Patients' on component 'Client Card'
 Then(
-  /^I should see text '([^"]*)' into element '([^"]*)' and text '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-  async (nameText, nameElement, mobileText, page, component) => {
-    const funcName = await getConvertTextToVariable(component);
-    const uniqueRowAncestor = await pageFactory(page.toLowerCase())[funcName](
-      mobileText
-    );
+    /^I should see text '([^"]*)' into element '([^"]*)' and text '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+    async (nameText, nameElement, mobileText, page, component) => {
+      const funcName = await getConvertTextToVariable(component);
+      const uniqueRowAncestor = await pageFactory(page.toLowerCase())[funcName](
+          mobileText,
+      );
 
-    const nameLocatorText = await uniqueRowAncestor
-      .item(nameElement.toLowerCase())
-      .getText();
-    await expect(nameLocatorText).toEqual(nameText);
-  }
+      const nameLocatorText = await uniqueRowAncestor
+          .item(nameElement.toLowerCase())
+          .getText();
+      await expect(nameLocatorText).toEqual(nameText);
+    },
 );
 
-Then(
-  /^I should see patients '([^"]*)' into elements '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-  async (text, element, page, component) => {
-    element = await getConvertTextToVariable(element);
-    const currentElement = await pageFactory(page)
-      .clientCard(this.name)
-      .item(element);
-    await checkElementIsDisplayed(currentElement);
-    const currentText = await currentElement.getText();
-    await expect(currentText).toEqual(text);
-  }
-);
+// Then(
+//     /^I should see patients '([^"]*)' into elements '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+//     async (text, element, page, component) => {
+//       element = await getConvertTextToVariable(element);
+//       const currentElement = await pageFactory(page)
+//           .clientCard(this.name)
+//           .item(element);
+//       await checkElementIsDisplayed(currentElement);
+//       const currentText = await currentElement.getText();
+//       await expect(currentText).toEqual(text);
+//     },
+// );
 
 Then(
-  /^I should see patient with mobile number '([^"]*)' on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
-  async (text, element, page, component) => {
-    this.patientMobile = text;
-    element = await getConvertTextToVariable(element);
-    const currentElement = await pageFactory(page)
-      .clientCard(text)
-      .item(element);
-    await clickElementWithWait(currentElement);
-  }
+    /^I should see patient with mobile number '([^"]*)' on the element '([^"]*)' on page '([^"]*)' on component '([^"]*)'$/,
+    async (text, element, page, component) => {
+      this.patientMobile = text;
+      element = await getConvertTextToVariable(element);
+      const currentElement = await pageFactory(page)
+          .clientCard(text)
+          .item(element);
+      await clickElementWithWait(currentElement);
+    },
 );
--- tests/features/patients.feature
+++ tests/features/patients.feature
@@ -73,24 +73,22 @@ Feature: User Flow on 'Patients' page
       #  And I click on the "Save" button
       #  Then I should see a new patient field
 
-    Scenario: Creating a new patient
+    # Scenario: Creating a new patient
 
-        When I click on the element 'Add New Patient' on page 'Patients' on component 'List Header'
-        When I enter 'Anna' into element 'Name' on page 'Patients' on component 'New Patient Modal'
-        When I click on the element 'genderFemale' on page 'Patients' on component 'New Patient Modal'
-        And  I enter '1111111111' into element 'Mobile' on page 'Patients' on component 'New Patient Modal'
-        And  I enter 'q@mail.ru' into element 'Email' on page 'Patients' on component 'New Patient Modal'
-        And  I click on 'AB+' element in 'Blood Group' list on page 'Patients' on component 'New Patient Modal'
-        And  I enter 'Stomachache' into element 'Symptoms' on page 'Patients' on component 'New Patient Modal'
-        When  I click on the element 'save' on page 'Patients' on component 'New Patient Modal'
-        Then I should see text 'Anna' into element 'Name' and text '(111) 111-1111' on page 'Patients' on component 'Client Card'
+    #     When I click on the element 'Add New Patient' on page 'Patients' on component 'List Header'
+    #     When I enter 'Anna' into element 'Name' on page 'Patients' on component 'New Patient Modal'
+    #     When I click on the element 'genderFemale' on page 'Patients' on component 'New Patient Modal'
+    #     And  I enter '1111111111' into element 'Mobile' on page 'Patients' on component 'New Patient Modal'
+    #     And  I enter 'q@mail.ru' into element 'Email' on page 'Patients' on component 'New Patient Modal'
+    #     And  I click on 'AB+' element in 'Blood Group' list on page 'Patients' on component 'New Patient Modal'
+    #     And  I enter 'Stomachache' into element 'Symptoms' on page 'Patients' on component 'New Patient Modal'
+    #     When  I click on the element 'save' on page 'Patients' on component 'New Patient Modal'
+    #     Then I should see text 'Anna' into element 'Name' and text '(111) 111-1111' on page 'Patients' on component 'Client Card'
 
 
-  #  Scenario: Check Patients Details
+   Scenario: Check Patients Details
 
-  #      When I enter '(717) 555-4445' into element 'Search Patient' on page 'Patients' on component 'List Header'
-  #      Then I should see patient with mobile number '(717) 555-4445' on the element 'Name Link' on page 'Patients' on component 'ClientCard'
-  #      Then I should see for patients 'Milka' into elements 'Name Link' on page 'Patients' on component 'Client Card'
-  #      And I click on 'Name' on page 'Patients' in the 'ClientCard'
-  #      Then I should see for patients 'Milka' into elements 'Name' on page 'Patients' on component 'Client Details'
-  #      When I click on the patient with mobile number '(717) 555-4445' on the element 'Name Link' on page 'Patients' on component 'Client Card'
+       When I enter '(717) 555-4445' into element 'Search Patient' on page 'Patients' on component 'List Header'
+       Then I should see patient with mobile number '(717) 555-4445' on the element 'Name Link' on page 'Patients' on component 'ClientCard'
+       Then I should see for patients 'Milka' into elements 'Name' on page 'Patients' on component 'Client Details'
+       
