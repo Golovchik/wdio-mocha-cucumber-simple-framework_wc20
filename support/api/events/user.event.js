35a3125694e0b6d63b87534b86ae597172f2d175
Vlad Golovchik <golo.vv@yandex.ru>
2023-02-15 00:27:28 +0300

    fix: removed comments and tbs

--- support/api/events/user.event.js
+++ support/api/events/user.event.js
@@ -1,51 +1,49 @@
-const { sendRequest } = require("../api.helper");
+const {sendRequest} = require('../api.helper');
 
 class UserEvent {
-  //constructor() {}
-
   static async createUser(data) {
-    const response = await sendRequest("post", "user", data);
+    const response = await sendRequest('post', 'user', data);
     return response;
   }
 
   static async getUser(userName) {
-    const response = await sendRequest("get", `user/${userName}`);
+    const response = await sendRequest('get', `user/${userName}`);
     return response;
   }
 
   static async createUserFromArray(data) {
-    const response = await sendRequest("post", "user/createWithArray", data);
+    const response = await sendRequest('post', 'user/createWithArray', data);
     return response;
   }
 
   static async updateUser(userName, data) {
-    const response = await sendRequest("put", `user/${userName}`, data);
+    const response = await sendRequest('put', `user/${userName}`, data);
     return response;
   }
 
   static async loginUser(userName, password) {
     const url = `user/login?username=${userName}&password=${password}`;
-    const response = await sendRequest("get", url);
+    const response = await sendRequest('get', url);
     return response;
   }
 
   static async logoutUser() {
-    const response = await sendRequest("get", "user/logout");
+    const response = await sendRequest('get', 'user/logout');
     return response;
   }
 
   static async deleteUser(userName) {
-    const response = await sendRequest("delete", `user/${userName}`);
+    const response = await sendRequest('delete', `user/${userName}`);
     return response;
   }
 
   static async createTestData(data) {
-    const response = await sendRequest("post", "user", data);
+    const response = await sendRequest('post', 'user', data);
     return response;
   }
 
   static async clearTestData(userName) {
-    const response = await sendRequest("delete", `user/${userName}`);
+    const response = await sendRequest('delete', `user/${userName}`);
     return response;
   }
 }
--- tests/specs/user.spec.js
+++ tests/specs/user.spec.js
@@ -1,10 +1,10 @@
-const { expect } = require("chai");
-const UserEvent = require("../../support/api/events/user.event");
-const { userTestData } = require("../testData/user.testData");
-const { validateResponse } = require("../../support/api/checkSchema");
-const { userSchema } = require("../../support/api/schemas/index");
+const {expect} = require('chai');
+const UserEvent = require('../../support/api/events/user.event');
+const {userTestData} = require('../testData/user.testData');
+const {validateResponse} = require('../../support/api/checkSchema');
+const {userSchema} = require('../../support/api/schemas/index');
 
-describe("API Test For USER", () => {
+describe('API Test For USER', () => {
   const data = userTestData.data;
   const failedData = userTestData.failedData;
 
@@ -17,63 +17,63 @@ describe("API Test For USER", () => {
     await UserEvent.clearTestData(userName);
   });
 
-  describe("POST Request", function () {
-    it("should successfully create user", async () => {
+  describe('POST Request', function() {
+    it('should successfully create user', async () => {
       const response = await UserEvent.createUser(data);
       const isCorrectResponse = await validateResponse(
-        userSchema.createUser,
-        response.data
+          userSchema.createUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
       expect(response.status).to.equal(200);
     });
 
-    it("should unsuccessfully create user", async () => {
+    it('should unsuccessfully create user', async () => {
       const response = await UserEvent.createUser([{}, {}]);
 
       expect(response.status).to.equal(500);
     });
 
-    it("should successfully create user from array", async () => {
+    it('should successfully create user from array', async () => {
       const arrayData = [data];
       const response = await UserEvent.createUserFromArray(arrayData);
       const isCorrectResponse = await validateResponse(
-        userSchema.createUserFromArray,
-        response.data
+          userSchema.createUserFromArray,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
       expect(response.status).to.equal(200);
-      expect(response.data.message).to.equal("ok");
+      expect(response.data.message).to.equal('ok');
     });
 
-    it("should unsuccessfully create user from array", async () => {
+    it('should unsuccessfully create user from array', async () => {
       const response = await UserEvent.createUserFromArray(failedData);
       expect(response.status).to.equal(500);
     });
   });
 
-  describe("GET Request", function () {
-    it("should successfully login", async () => {
+  describe('GET Request', function() {
+    it('should successfully login', async () => {
       const userName = data.username;
       const password = data.password;
       const response = await UserEvent.loginUser(userName, password);
       const isCorrectResponse = await validateResponse(
-        userSchema.loginUser,
-        response.data
+          userSchema.loginUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
       expect(response.status).to.equal(200);
     });
 
-    it("should successfully get user", async () => {
+    it('should successfully get user', async () => {
       const userName = data.username;
       const response = await UserEvent.getUser(userName);
       const isCorrectResponse = await validateResponse(
-        userSchema.getUser,
-        response.data
+          userSchema.getUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
@@ -81,52 +81,52 @@ describe("API Test For USER", () => {
       expect(response.data.username).to.equal(userName);
     });
 
-    it("should unsuccessfully get user", async () => {
+    it('should unsuccessfully get user', async () => {
       const userName = failedData.username;
       const response = await UserEvent.getUser(userName);
 
       expect(response.status).to.equal(404);
     });
 
-    it("should successfully logout", async () => {
+    it('should successfully logout', async () => {
       const response = await UserEvent.logoutUser();
       const isCorrectResponse = await validateResponse(
-        userSchema.logoutUser,
-        response.data
+          userSchema.logoutUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
       expect(response.status).to.equal(200);
-      expect(response.data.message).to.equal("ok");
+      expect(response.data.message).to.equal('ok');
     });
   });
 
-  describe("PUT Request", function () {
-    it("should successfully update user", async () => {
+  describe('PUT Request', function() {
+    it('should successfully update user', async () => {
       const response = await UserEvent.updateUser(data.username, data);
       const isCorrectResponse = await validateResponse(
-        userSchema.updateUser,
-        response.data
+          userSchema.updateUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
       expect(response.status).to.equal(200);
     });
 
-    it("should unsuccessfully update user", async () => {
-      const response = await UserEvent.updateUser("", failedData);
+    it('should unsuccessfully update user', async () => {
+      const response = await UserEvent.updateUser('', failedData);
 
       expect(response.status).to.equal(405);
     });
   });
 
-  describe("DELETE Request", function () {
-    it("should successfully delete user", async () => {
+  describe('DELETE Request', function() {
+    it('should successfully delete user', async () => {
       const userName = data.username;
       const response = await UserEvent.deleteUser(userName);
       const isCorrectResponse = await validateResponse(
-        userSchema.deleteUser,
-        response.data
+          userSchema.deleteUser,
+          response.data,
       );
 
       expect(isCorrectResponse).to.be.equal(true);
@@ -134,7 +134,7 @@ describe("API Test For USER", () => {
       expect(response.data.message).to.equal(userName);
     });
 
-    it("should unsuccessfully delete user", async () => {
+    it('should unsuccessfully delete user', async () => {
       const userName = failedData.username;
       const response = await UserEvent.deleteUser(userName);
 
