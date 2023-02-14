7d58effa27409c8233d408ae3167ecd4fc949e48
Vlad Golovchik <golo.vv@yandex.ru>
2023-02-14 09:09:02 +0300

    feat: added get api

--- tests/specs/user.spec.js
+++ tests/specs/user.spec.js
@@ -1,10 +1,71 @@
 const {expect} = require('chai');
-const {sendRequest} = require('../../support/api/api.helper');
+const UserEvent = require('../../support/api/events/user.event');
+const {userTestData} = require('../testData/user.testData');
 
-describe('API Test Suite USER', () => {
-  it('should get', async () => {
-    const response = await sendRequest('get', `user/Vlad`);
+describe('API Test For USER', () => {
+  const data = userTestData.data;
+  const failedData = userTestData.failedData;
 
-    expect(response.status).to.equal(200);
+  describe.skip('POST Request', function() {
+    this.retries(10);
+
+    it('should successfully create user', async () => {
+      const response = await UserEvent.createUser(data);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should unsuccessfully create user', async () => {
+      const response = await UserEvent.createUser(failedData);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should successfully create user from array', async () => {
+      const response = await UserEvent.createUserFromArray(data);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should unsuccessfully create user from array', async () => {
+      const response = await UserEvent.createUserFromArray(failedData);
+
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  describe('GET Request', function() {
+    this.retries(10);
+
+    it('should successfully login', async () => {
+      const username = userTestData.data.username;
+      const password = userTestData.data.password;
+
+      const response = await UserEvent.loginUser(username, password);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should successfully get user', async () => {
+      const username = userTestData.data.username;
+
+      const response = await UserEvent.getUser(username);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should unsuccessfully get user', async () => {
+      const username = userTestData.failedData.username;
+
+      const response = await UserEvent.getUser(username);
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('should successfully logout', async () => {
+      const response = await UserEvent.logoutUser();
+
+      expect(response.status).to.equal(200);
+    });
   });
 });
---
+++ tests/testData/user.testData.js
@@ -0,0 +1,26 @@
+const userTestData = {
+  data: {
+    id: 1037,
+    username: 'Uladzislau',
+    firstName: 'Uladzislau',
+    lastName: 'HALOUCHYK',
+    email: 'Uladzislau@yan.con',
+    password: 'password',
+    phone: '25654858',
+    userStatus: 2,
+  },
+  failedData: {
+    id: 35636,
+    username: 'failed_Uladzislau',
+    firstName: 'failed_Uladzislau',
+    lastName: 'failed_HALOUCHYK',
+    email: 'failed_Uladzislau@yan.con',
+    password: 'failed_password',
+    phone: '256456654858',
+    userStatus: 0,
+  },
+};
+
+module.exports = {
+  userTestData,
+};
